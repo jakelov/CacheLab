36ff167e550f97e6360ed3858047b4320e4c80d2
jakelov <56813935+jakelov@users.noreply.github.com>
2020-10-09 18:21:00 -0400

    Update csim.c

--- csim.c
+++ csim.c
@@ -4,9 +4,25 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <math.h>
+/**
+ *  
+ *   Jacob Lovingood, Spencer Withee
+ *
+ *   lovingoodja1, witheesp
+ *  
+ */
 
+// Verbose flag used for verbose output.
 int verbose = 0;
 
+/** Cache holds different variables for abstraction of a cache.
+ * Associativity: Number of lines per set (slots).
+ * Index Bits: Equalivalent to the s in 2^s for the number of sets.
+ * Block Bits: Number of bits for a block.
+ * Block Size: The number 2^b where b is the number of bits per block.
+ * Sets: The number 2^s which is the number of sets in the cache.
+ * Tags is a ref to number of tags.
+ */
 typedef struct Cache
 {
     unsigned long int sets;
@@ -17,6 +33,9 @@ typedef struct Cache
     unsigned long int **tags;
 }Cache;
 
+/**
+ * Print for help calling and testing the Cache.
+*/
 void printUsage()
 {
     printf("Usage: ./csim-ref [-h] [-v] -s <s> -E <E> -b <b> -t <tracefile>\n");
@@ -28,6 +47,9 @@ void printUsage()
     printf("-t <tracefile>: Name of the valgrind trace to replay\n");
 }
 
+/**
+ * Helper function to get a range of bits.
+*/
 unsigned long getBits(unsigned long source, int low, int high)
 {
     if (low < 0 || high > 63)
@@ -42,6 +64,9 @@ unsigned long getBits(unsigned long source, int low, int high)
     }
 }
 
+/**
+ * Inserts at the front of the array to continue the trend that tags[setindex][0] is the LRU line
+*/
 void insertAtFront(Cache *cache, int set_index, int index)
 {
     unsigned long int temp_tags[cache->sets][cache->associativity];
@@ -71,6 +96,9 @@ void insertAtFront(Cache *cache, int set_index, int index)
 
 }
 
+/**
+ * Function to access the cache and determine if the access is a hit, miss, or eviction
+*/
 void accessCache(Cache *cache, unsigned long address, int *hit_count, int *miss_count, int *eviction_count)
 {
 
@@ -148,7 +176,9 @@ void accessCache(Cache *cache, unsigned long address, int *hit_count, int *miss_
     
     return; 
 }
-
+/**
+ * Main Method to run the program, determine what type of operation is occuring, and perform the operation. Also does cache setup/global flag initialization.
+*/
 int main(int argc, char** argv)
 {
     Cache cache;
@@ -159,16 +189,17 @@ int main(int argc, char** argv)
     int option = 0;
 
     char *traceFile;
-
+    // Determine what arguments were passed.
     while ((option = getopt(argc, argv, "hvs:E:b:t:")) != -1)
     {
+        // Initialization of fields.
         switch(option)
         {
             case 'h':
                 printUsage();
                 exit(0);
             case 'v':
-                verbose = 1;
+                verbose = 1; // verbose flag.
                 break;
             case 's':
                 cache.index_bits = atoi(optarg);
@@ -186,17 +217,17 @@ int main(int argc, char** argv)
                 exit(1);
         }
     }
-
+    // Field Initialization
     cache.sets = pow(2.0, cache.index_bits);
     cache.block_size = pow(2.0, cache.block_bits);
-    
+    // Array Allocation
     cache.tags = malloc(cache.sets * sizeof(unsigned long int *));
-    
+  
     for (int i = 0; i < cache.sets; i++)
     {
         cache.tags[i] = malloc(cache.associativity * sizeof(unsigned long int));
     }
-
+    // initialization of the tags for validity and checking for need to eviction in access.
     for (int j = 0; j < cache.sets; j++)
     {
         for (int k = 0; k < cache.associativity; k++)
@@ -204,13 +235,13 @@ int main(int argc, char** argv)
             cache.tags[j][k] = -1;
         }
     }
-
+    // File opening/reading.
     FILE *file = fopen(traceFile, "r");
 
     char operation;
     unsigned long address;
     int size;
-
+    // Scan the file and initialize the operation/address/size fields.
     if (file != NULL)
     {
         while(fscanf(file, " %c %lx,%d", &operation, &address, &size) == 3)
@@ -263,7 +294,7 @@ int main(int argc, char** argv)
         }
     }
 
-
+    // Print and close.
     printSummary(hit_count, miss_count, eviction_count);
     fclose(file);
     return 0;
